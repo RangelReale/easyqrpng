829f021b7107f3ff92a575b66ef91ede096e5b5a
Rangel Reale <rangelspam@gmail.com>
2013-10-02 13:39:17 -0300

    * Better error checking in sample

--- sample/main.cpp
+++ sample/main.cpp
@@ -7,13 +7,13 @@ int main(int argc, char* argv[])
 	easyqrpng p;
 	p.setTargetWidth(500);
 
-	if (p.encode("Encode me in a test"))
+	if (p.encode("Encode me in a test")!=EASYQRPNGERR_OK)
 	{
 		std::cout << "ENCODE ERROR: " << p.errorMessage() << std::endl;
 		return 0;
 	}
 
-	if (p.save("test.png"))
+	if (p.save("test.png")!=EASYQRPNGERR_OK)
 	{
 		std::cout << "SAVE ERROR: " << p.errorMessage() << std::endl;
 		return 1;
