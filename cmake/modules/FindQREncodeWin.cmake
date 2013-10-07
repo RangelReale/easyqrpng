791d3302dbed97a20ee5085c12100415c40736b7
Rangel Reale <rangelspam@gmail.com>
2013-10-07 12:02:31 -0300

    * Linux support

--- cmake/modules/FindQREncodeWin.cmake
+++ cmake/modules/FindQREncodeWin.cmake
@@ -4,19 +4,19 @@ SET(QREncodeWin_FOUND 0)
 
 IF(QREncodeWin_ROOT_DIR)
   IF(EXISTS "${QREncodeWin_ROOT_DIR}/qrencode.h")
-        SET(QREncodeWin_INCLUDE_DIR "${QREncodeWin_ROOT_DIR}")
+        SET(QRENCODE_INCLUDE_DIR "${QREncodeWin_ROOT_DIR}")
         SET(QREncodeWin_FOUND 1)
   ELSE()
       MESSAGE(FATAL_ERROR "QREncodeWin was not found.")
   ENDIF()
   
   IF(QREncodeWin_FOUND)
-        FIND_LIBRARY(QREncodeWin_LIBRARY_DEBUG NAMES qrcodelib.lib)
-        FIND_LIBRARY(QREncodeWin_LIBRARY_RELEASE NAMES qrcodelib.lib)
+        FIND_LIBRARY(QRENCODE_LIBRARY_DEBUG NAMES qrcodelib.lib)
+        FIND_LIBRARY(QRENCODE_LIBRARY_RELEASE NAMES qrcodelib.lib)
         
-        SET(QREncodeWin_LIBRARY
-          debug ${QREncodeWin_LIBRARY_DEBUG}
-          optimized ${QREncodeWin_LIBRARY_RELEASE}
+        SET(QRENCODE_LIBRARY
+          debug ${QRENCODE_LIBRARY_DEBUG}
+          optimized ${QRENCODE_LIBRARY_RELEASE}
         )        
   ENDIF()
 ENDIF()
